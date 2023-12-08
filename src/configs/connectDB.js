bd078824102d33c4721e876138309cec175552c3
vuduchiieu <vuduchiieu@gmail.com>
2023-12-08 21:09:39 +0700

    fix bug

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,9 +1,11 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    database: "nodejsbasic",
+    host: "brdyncj8vrxm9fkfvbjx-mysql.services.clever-cloud.com",
+    user: "uvgblflufelbe28r",
+    password: "F63pHZMEefidpmEGuvWq",
+    database: "brdyncj8vrxm9fkfvbjx",
+    port: 3306, // Clever Cloud MySQL port
 });
 
 export default pool;
